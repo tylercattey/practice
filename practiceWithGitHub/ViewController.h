8582370505a63a87f430aafc678ec0c114b5a0ca
Tyler Cattey <Tylercattey@me.com>
2013-05-03 08:33:32 -0600

    Initial Commit

---
+++ practiceWithGitHub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  practiceWithGitHub
+//
+//  Created by Student on 5/3/13.
+//  Copyright (c) 2013 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
